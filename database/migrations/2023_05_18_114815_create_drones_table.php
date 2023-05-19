6fa50b5f760ed110b3d84a5b5e126e45ecbd5b78
THOEUN.SIENGDAV <THOEUN.SIENGDAV@PNC.LAN>
2023-05-19 18:59:14 +0700

    create relationship between user and drone

--- database/migrations/2023_05_18_114815_create_drones_table.php
+++ database/migrations/2023_05_18_114815_create_drones_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('dateTime');
             $table->string('image');
             $table->string('battery');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
