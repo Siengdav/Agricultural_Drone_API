97320ffcf7a517dce6d43a29be464bb8008bb407
doeur100 <dietdoeur@gmail.com>
2023-05-19 11:51:13 +0700

    create Users, Locations, Maps(table,model,controller,request)

--- database/migrations/2023_05_19_035512_create_locations_table.php
+++ database/migrations/2023_05_19_035512_create_locations_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id("user_id");
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id('location_id');
+            $table->string('province');
+            $table->string('road');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('locations');
     }
 };
--- database/migrations/2023_05_19_040131_create_maps_table.php
+++ database/migrations/2023_05_19_040131_create_maps_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id("user_id");
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('maps', function (Blueprint $table) {
+            $table->id('map_id');
+            $table->string('latitude');
+            $table->string('longitude');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('maps');
     }
 };
